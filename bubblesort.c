26b6de42a157618a9550344644f9cf4330b24160
Athira Renjith <athirarenjith2003@gmail.com>
2024-08-18 12:24:43 +0530

    Update bubblesort.c

--- bubblesort.c
+++ bubblesort.c
@@ -1,24 +1,44 @@
 #include <stdio.h>
-
 int main()
-{   int a[50],n,i,j,temp;
+{   int a[50],n,i,j,temp,t;
     printf("Enter no. of elements of array:");
+    t++;
     scanf("%d",&n);
+    t++;
     printf("Enter elements of array:");
+    t++;
     for(i=0;i<n;i++){
+        t++;
         scanf("%d",&a[i]);
+        t++;
     }
+    t++;
     for(i=0;i<n-1;i++){
+        t++;
         for(j=0;j<n-i-1;j++){
+            t++;
             if(a[j]>a[j+1]){
+                t++;
                 temp=a[j];
                 a[j]=a[j+1];
                 a[j+1]=temp;
+                t++;
+                t++;
+                t++;
             }
-        }
+        }t++;
     }
+    t++;
     printf("Sorted array is: ");
+    t++;
     for(i=0;i<n;i++){
+        t++;
+        t++;
         printf("%d \t",a[i]);
     }
+    t++;
+    printf("\nSpace complexity =%d \n",(5*4+n*4));
+    t++;
+    t++;
+    printf("Time complexity = %d",t);
 }
